c3b0b55c2e7d1703dd6b15e3e726c73e18ee2a20
Brian Balderston <brian.j.balderston.civ@mail.mil>
2019-11-07 07:39:33 -0500

    Add helper to store comments in session

---
+++ tutorial-code-razor/SessionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using tutorial_code_razor.Models;
+
+namespace tutorial_code_razor
+{
+    public static class SessionExtensions
+    {
+        public static List<CommentModel> GetComments(this ISession session, string key)
+        {
+            var data = session.Get(key);
+
+            if (data == null)
+            {
+                return null;
+            }
+
+            var ms = new MemoryStream(data);
+            var bf = new BinaryFormatter();
+
+            return (List<CommentModel>)bf.Deserialize(ms);
+        }
+        public static void SetComments(this ISession session, string key, List<CommentModel> value)
+        {
+            var ms = new MemoryStream();
+            var bf = new BinaryFormatter();
+
+            bf.Serialize(ms, value);
+
+            ms.Close();
+
+            session.Set(key, ms.ToArray());
+        }
+    }
+}
